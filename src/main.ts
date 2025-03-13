1104cc65d0f04adc3efc6029fc3567bb0931ca43
Nagendra <nagendrakushwahaplus99.com>
2025-03-13 12:17:15 +0530

    create animation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent,{
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(MatMenuModule)
+  ]})
   .catch((err) => console.error(err));
